889f96eca2092bde0c63386b943e07f84f3ce162
sebastian-md <sebas.adrv@gmail.com>
2023-03-05 20:43:57 +0100

    vUno

---
+++ SebastianJava46.java
@@ -0,0 +1,7 @@
+public class SebastianJava46 {
+
+	public static void main(String[] args) {		
+		System.out.println("Me llamo Sebastian y he creado el servidor ubunserver46 y vivo en Madrid");
+	}
+
+}
